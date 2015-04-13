4567f6008bbb17a916252ce1476761128200de28
Ivan Podorozhny <ivan@podorozhny.ru>
2015-04-13 21:52:09 +0300

    small changes

--- web/app_prod.php
+++ web/app_prod.php
@@ -3,14 +3,6 @@
 //use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1',])
-	&& substr($_SERVER['REMOTE_ADDR'], 0, 11) !== '192.168.100'
-    && substr($_SERVER['REMOTE_ADDR'], 0, 7) !== '178.167'
-) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('Get the fuck outta here!');
-}
-
 $loader = require_once __DIR__ . '/../app/bootstrap.php.cache';
 
 // Enable APC for autoloading to improve performance.
